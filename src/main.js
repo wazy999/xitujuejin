0109c068954f59fb25b8ccea250da442dbb47c3e
lee <759757929@qq.com>
2019-11-25 22:57:22 +0800

    上传

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
-import VueRouter from 'vue-router';
 import router from "./router"
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
-Vue.use(VueRouter)
 
 Vue.use(ElementUI);
 new Vue({
